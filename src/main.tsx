2d23766b6bb2d954044eef1bdab299795be65092
Mateusz Skopowski <mateusz.skopowski@gmail.com>
2023-06-05 10:11:43 +0200

    Add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+import { inject } from "@vercel/analytics";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./ui/app";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
